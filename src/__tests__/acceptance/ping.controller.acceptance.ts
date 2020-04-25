ae6a65bf7102bfdd3983728a610d9ad7f62ecc0d
ishu <sandeep.singh@sourcefuse.com>
2020-04-25 13:29:26 +0530

    test3

--- src/__tests__/acceptance/ping.controller.acceptance.ts
+++ src/__tests__/acceptance/ping.controller.acceptance.ts
@@ -14,7 +14,7 @@ describe('PingController', () => {
     await app.stop();
   });
 
-  it('invokes GET /ping', async () => {
+  it('invokes GETx /ping', async () => {
     const res = await client.get('/ping?msg=world').expect(200);
     expect(res.body).to.containEql({greeting: 'Hello from LoopBack'});
   });
